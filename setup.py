4f5ec7bbd5af8767fe97e0ee26e89768f666bdab
HETVI996 <kakkadhetvi87@gmail.com>
2025-11-20 12:30:55 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    get_requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
+
+setup(
+    name="NLDS-ML",
+    version="0.0.1",
+    author="Hetvi Kakkad",
+    author_email="kakkadhetvi87@gmail.com",
+    description="A machine learning package.",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    
+)
